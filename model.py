3bdfcc860c0544737b018f48faf5102b4f73610d
weiyinwei <weiyinwei@hotmail.com>
2019-12-03 12:09:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ from torch_geometric.nn import SAGEConv, GATConv
 
 
 class Net(torch.nn.Module):
-    def __init__(self, features, uh_edge_index, v_uh_edge_index, batch_size, num_user, num_hashtag, num_video, dim_latent, num_heads=1, aggr='mean'):
+    def __init__(self, features, uh_edge_index, v_uh_edge_index, batch_size, num_user, num_hashtag, num_video, dim_latent, aggr='mean'):
         super(Net, self).__init__()
         self.batch_size = batch_size
         self.num_user = num_user
@@ -30,34 +30,19 @@ class Net(torch.nn.Module):
         self.u_h_embedding = nn.init.xavier_normal_(torch.rand((num_user+num_hashtag, self.dim_latent), requires_grad=True)).cuda()
         self.video_features = torch.tensor(features, dtype=torch.float).cuda()
         self.trans_video_layer = nn.Linear(self.dim_feat, self.dim_latent)
-        nn.init.xavier_normal_(self.trans_video_layer.weight)
-
         self.result_embed = nn.init.xavier_normal_(torch.rand((num_user+num_hashtag, self.dim_latent))).cuda()
 
-        self.con_1 = GATConv(self.dim_latent, self.dim_latent, heads=num_heads, concat=False)
-        nn.init.xavier_normal_(self.con_1.weight) 
-        self.con_3 = SAGEConv(self.dim_latent, self.dim_latent, self.aggr)
-        nn.init.xavier_normal_(self.con_3.weight)   
+        self.first_conv = GATConv(self.dim_latent, self.dim_latent, self.aggr)
+        nn.init.xavier_normal_(self.first_conv.weight)      
+        self.second_conv = SAGEConv(self.dim_latent, self.dim_latent, self.aggr)
+        nn.init.xavier_normal_(self.second_conv.weight)
+        self.third_conv = GATConv(self.dim_latent, self.dim_latent, self.aggr)
+        nn.init.xavier_normal_(self.third_conv.weight)
+        self.forth_conv = SAGEConv(self.dim_latent, self.dim_latent, self.aggr)
+        nn.init.xavier_normal_(self.forth_conv.weight)
 
-        self.con_4 = GATConv(self.dim_latent, self.dim_latent, heads=num_heads, concat=False)  
-        nn.init.xavier_normal_(self.con_4.weight)      
-        self.con_6 = SAGEConv(self.dim_latent, self.dim_latent, self.aggr)
-        nn.init.xavier_normal_(self.con_6.weight)      
-
-        self.con_7 = GATConv(self.dim_latent, self.dim_latent, heads=num_heads, concat=False)
-        nn.init.xavier_normal_(self.con_7.weight)      
-        self.con_9 = SAGEConv(self.dim_latent, self.dim_latent, self.aggr)
-        nn.init.xavier_normal_(self.con_9.weight)     
-
-        self.weight_v = nn.init.xavier_normal_(torch.rand((self.dim_latent, self.dim_latent), requires_grad=True)).cuda()
-        self.weight_h = nn.init.xavier_normal_(torch.rand((self.dim_latent, self.dim_latent), requires_grad=True)).cuda()
-        self.weight_v_u = nn.init.xavier_normal_(torch.rand((self.dim_latent, self.dim_latent), requires_grad=True)).cuda()
-        self.weight_h_u = nn.init.xavier_normal_(torch.rand((self.dim_latent, self.dim_latent), requires_grad=True)).cuda()        
-        self.bias_v = torch.zeros((self.dim_latent), requires_grad=True).cuda()
-        self.bias_h = torch.zeros((self.dim_latent), requires_grad=True).cuda()
-
-        # self.user_video_layer = nn.Linear(2*self.dim_latent, self.dim_latent)
-        # self.user_hashtag_layer = nn.Linear(2*self.dim_latent, self.dim_latent)
+        self.user_video_layer = nn.Linear(2*self.dim_latent, self.dim_latent)
+        self.user_hashtag_layer = nn.Linear(2*self.dim_latent, self.dim_latent)
 
     def forward(self, item):        
         user_tensor = item[:,[0]]
@@ -65,20 +50,18 @@ class Net(torch.nn.Module):
         pos_hashtag_tensor = item[:,[2]]
         neg_hashtag_tensor = item[:,[3]]
         x = F.leaky_relu(self.trans_video_layer(self.video_features))
-        id_embedding = torch.cat((self.u_h_embedding, torch.zeros_like(x).cuda()), dim=0)
         x = torch.cat((self.u_h_embedding, x), dim=0)
-        id_embedding = F.normalize(id_embedding)
         x =  F.normalize(x)
 
-        x = F.leaky_relu(self.con_1(x, self.v_uh_edge_index))
-        x = F.leaky_relu(self.con_3(x, self.uh_edge_index))
-
-        x = F.leaky_relu(self.con_4(x, self.v_uh_edge_index))
-        x = F.leaky_relu(self.con_6(x, self.uh_edge_index))
+        x = F.leaky_relu(self.first_conv(x, self.v_uh_edge_index))
+        x = F.leaky_relu(self.second_conv(x, self.uh_edge_index))
+        x = F.leaky_relu(self.third_conv(x, self.v_uh_edge_index))
+        x = F.leaky_relu(self.forth_conv(x, self.uh_edge_index))
+        # x = F.leaky_relu(self.first_conv(x, self.v_uh_edge_index))
+        # x = F.leaky_relu(self.second_conv(x, self.uh_edge_index))
+        # x = F.leaky_relu(self.first_conv(x, self.v_uh_edge_index))
+        # x = F.leaky_relu(self.second_conv(x, self.uh_edge_index))
         
-        x = F.leaky_relu(self.con_7(x, self.v_uh_edge_index))
-        x = F.leaky_relu(self.con_9(x, self.uh_edge_index))
-
         self.result_embed = x[torch.arange(self.num_user+self.num_hashtag).cuda()]
 
         user_tensor = self.result_embed[user_tensor].squeeze(1)
@@ -87,12 +70,9 @@ class Net(torch.nn.Module):
 
         video_tensor = self.video_features[video_tensor-self.num_user-self.num_hashtag].squeeze(1)
         video_tensor = F.leaky_relu(self.trans_video_layer(video_tensor))
-        # user_specific_video = F.leaky_relu(self.user_video_layer(torch.cat((video_tensor, user_tensor), dim=1)))
-        # user_specific_pos_h = F.leaky_relu(self.user_hashtag_layer(torch.cat((pos_hashtags_tensor, user_tensor), dim=1)))
-        # user_specific_neg_h = F.leaky_relu(self.user_hashtag_layer(torch.cat((neg_hashtags_tensor, user_tensor), dim=1)))
-        user_specific_video = F.leaky_relu(torch.matmul(video_tensor, self.weight_v)+torch.matmul(user_tensor, self.weight_v_u)+self.bias_v)
-        user_specific_pos_h = F.leaky_relu(torch.matmul(pos_hashtags_tensor, self.weight_h)+torch.matmul(user_tensor, self.weight_h_u)+self.bias_h)
-        user_specific_neg_h = F.leaky_relu(torch.matmul(neg_hashtags_tensor, self.weight_h)+torch.matmul(user_tensor, self.weight_h_u)+self.bias_h)
+        user_specific_video = F.leaky_relu(self.user_video_layer(torch.cat((video_tensor, user_tensor), dim=1)))
+        user_specific_pos_h = F.leaky_relu(self.user_hashtag_layer(torch.cat((pos_hashtags_tensor, user_tensor), dim=1)))
+        user_specific_neg_h = F.leaky_relu(self.user_hashtag_layer(torch.cat((neg_hashtags_tensor, user_tensor), dim=1)))
 
         pos_scores = torch.sum(user_specific_video*user_specific_pos_h, dim=1)
         neg_scores = torch.sum(user_specific_video*user_specific_neg_h, dim=1)
@@ -143,6 +123,20 @@ class Net(torch.nn.Module):
             pos_scores = torch.sum(user_specific_video*user_specific_pos_h, dim=1)
             neg_scores = torch.sum(user_specific_video*user_specific_neg_h, dim=1)
 
+            _, index_of_rank_list = torch.topk(torch.cat((neg_scores, pos_scores)), topk)
+            index_set = set([iofr.cpu().item() for iofr in index_of_rank_list])
+            num_hit = len(index_set.difference(all_set))
+            sum_pre += float(num_hit/topk)
+            sum_recall += float(num_hit/num_pos)
+            idcg = np.sum(1/np.log2(np.arange(min(num_pos, topk))+2))
+            ndcg_score = 0.0
+            for i in range(num_pos):
+                label_pos = neg_num + i
+                if label_pos in index_of_rank_list:
+                    index = list(index_of_rank_list.cpu().numpy()).index(label_pos)
+                    ndcg_score += 1 / math.log(index + 2)
+
+            sum_ndcg += ndcg_score/idcg
         bar.close()
 
         return sum_pre/sum_item, sum_recall/sum_item
